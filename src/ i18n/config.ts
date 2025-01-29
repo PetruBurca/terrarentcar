b6fe7690d28820c0354bfebabbcc9f793e8cbc0b
Max G <user@MacBook-Pro.local>
2025-01-30 00:24:20 +0200

    lastv

--- src/ i18n/config.ts
+++ src/ i18n/config.ts
@@ -1,31 +1,32 @@
-import i18next from "i18next"; 
-import { initReactI18next } from "react-i18next";
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import Backend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
-import LanguageDetector from "i18next-browser-languagedetector";
+const i18nInstance = i18n.createInstance();
 
-
-
-i18next
+i18nInstance
+  .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    lng: "ro",
-    fallbackLng: "ro",
+    lng: 'ro',
+    fallbackLng: 'ro',
     debug: false,
     interpolation: {
       escapeValue: false,
     },
     backend: {
-      loadPath: "/locales/{{lng}}/{{lng}}.json",
+      loadPath: '/locales/{{lng}}/{{lng}}.json',
     },
     detection: {
-      order: ["querystring", "cookie", "localStorage", "navigator"],
-      lookupQuerystring: "lng",
-      lookupCookie: "i18next",
-      lookupLocalStorage: "i18nextLng",
-      caches: ["localStorage", "cookie"],
-      convertDetectedLanguage: (lng: string) => (lng === "ru" ? "ru" : "ro"),
+      order: ['querystring', 'cookie', 'localStorage', 'navigator'],
+      lookupQuerystring: 'lng',
+      lookupCookie: 'i18next',
+      lookupLocalStorage: 'i18nextLng',
+      caches: ['localStorage', 'cookie'],
+      convertDetectedLanguage: (lng: string) => (lng === 'ru' ? 'ru' : 'ro'),
     },
   });
 
-export default i18next;
+export default i18nInstance;
