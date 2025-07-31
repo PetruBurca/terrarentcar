2df42d7aceccd2b62f71741ce17125a00f1a457b
KichZero <kichmg22@gmail.com>
2025-07-31 20:04:39 +0300

    fix: исправлены пути к ресурсам в index.html для корректной работы на GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { copy } from "fs-extra";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/" : "/",
   server: {
     host: "::",
     port: 8080,
